52945ee460bfcf6c3fb2ab62d89c6680b099a60d
Sam Crang <sam.crang@gmail.com>
2016-11-14 14:24:37 +0000

    Add example log lines

--- s3/node.js/test/s3_test.js
+++ s3/node.js/test/s3_test.js
@@ -4,11 +4,13 @@ var sinon = require('sinon');
 var AWS = require('aws-sdk-mock');
 var s3_lambda = require('../s3.js');
 
+var log = "#Version: 1.0\n#Fields: date time x-edge-location sc-bytes c-ip cs-method cs(Host) cs-uri-stem sc-status cs(Referer) cs(User-Agent) cs-uri-query cs(Cookie) x-edge-result-type x-edge-request-id x-host-header cs-protocol cs-bytes time-taken x-forwarded-for ssl-protocol ssl-cipher x-edge-response-result-type\n2016-02-09  12:31:48  SEA32 398 10.0.123.456  HEAD  fake-distribution.cloudfront.net  / 200 - Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36  - - Hit MRVMF7KydIvxMWfJIglgwHQwZsbG2IhRJ07sn9AkKUFSHS9EXAMPLE==  www.example.com https 369 0.002 - TLSv1.2 ECDHE-RSA-AES128-GCM-SHA256 Hit\n2016-02-09  12:31:48  SEA32 444 10.0.123.456  HEAD  fake-distribution.cloudfront.net  /something  200 - Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36  - - Hit MRVMF7KydIvxMWfJIglgwHQwZsbG2IhRJ07sn9AkKUFSHS9EXAMPLE==  www.example.com https 376 0.001 - TLSv1.2 ECDHE-RSA-AES128-GCM-SHA256 Hit\n"
+
 describe('s3_lambda', function() {
   describe('handler', function() {
     it('should do something', function(done) {
       var http_mock = nock('https://www.example.com')
-        .post("/hello", 'lol')
+        .post("/hello", log)
         .reply(200);
 
       var cb = function(assertions) {
@@ -24,7 +26,7 @@ describe('s3_lambda', function() {
         }
       };
 
-      AWS.mock('S3', 'getObject', new Buffer('lol'));
+      AWS.mock('S3', 'getObject', new Buffer(log));
 
       var stub = {bucket: "some-bucket", key: "some-key"};
 
